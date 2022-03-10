897e49108b55a4c4d5144fd5008dc6171a6d0934
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-10 09:50:35 +0600

    fixing minor bugs

--- music error/script.js
+++ music error/script.js
@@ -1,17 +1,21 @@
 const elementById = (id) => {
-  document.getElementById(id);
+  return document.getElementById(id);
 };
 
 const handleSearch = () => {
   const keyword = elementById("keyword");
+  
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
+  console.log(keyword.value);
+  console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  const artistContainer = elementById("artists");
+  // console.log(artistContainer);
   data?.artists?.forEach((artist) => {
     const div = document.createElement("div");
     div.classList.add("artist-card");
@@ -37,28 +41,29 @@ const showArtists = (data) => {
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com//api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
-    .then((data) => showAlbum(data));
+    .then(res => res.json())
+    .then(data => showAlbum(data));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
 };
 
 const showAlbum = (data) => {
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  console.log(data.album);
+  data.album.forEach((item) => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${data.album.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${data.album.strAlbum}</h3>
         </div>
       `;
 
