f63df9222a60a15c342a38e1d6fad75048ea92b9
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-10 10:23:23 +0600

    bug fix

--- music error/script.js
+++ music error/script.js
@@ -6,8 +6,8 @@ const handleSearch = () => {
   const keyword = elementById("keyword");
   
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
-  console.log(keyword.value);
-  console.log(url);
+  // console.log(keyword.value);
+  // console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
@@ -22,15 +22,15 @@ const showArtists = (data) => {
     div.innerHTML = `<div class="image-container">
     <div class="image-container-inner">
       <img
-        src="${artist.strArtistThumb}"
+        src="${artist.strArtistThumb ? artist.strArtistThumb : "https://image.shutterstock.com/image-vector/no-image-available-vector-illustration-260nw-744886198.jpg"}"
         alt=""
       />
     </div>
   </div>
   <div class="info-container">
     <h1>${artist.strArtist}</h1>
-    <p>Country: ${artist.strCountry}</p>
-    <p>Style: ${artist.strGenre}</p>
+    <p>Country: ${artist.strCountry ? artist.strCountry : "Not Available"}</p>
+    <p>Style: ${artist.strGenre ? artist.strGenre : "Not Available"}</p>
   </div>
   <button class="album-button">
     <i class="fa-solid fa-compact-disc"></i>
@@ -52,18 +52,20 @@ const fetchAlbums = (id) => {
 const showAlbum = (data) => {
   const albumContainer = elementById("albums");
   console.log(data.album);
+  console.log(data.album.strAlbumStripped);
   data.album.forEach((item) => {
+    console.log(item);
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${data.album.strAlbumThumb}"
+            src="${item.strAlbumThumb ? item.strAlbumThumb : "https://image.shutterstock.com/image-vector/no-image-available-vector-illustration-260nw-744886198.jpg"}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${data.album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
